9fe5e76d80127f2abc6925af08194ff06415945a
Yomy Gesese <yomygesese@gmail.com>
2022-09-12 02:48:59 -0700

    python exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def afe_print_division(a, b):
-        try:
-            div = a / b
-        except (TypeError, ZeroDivisionError):
-            div = None
-        finally:
-            print("Inside result: {}".format(div))
-        return (div)
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
