5167fd8e81d6f96d54f52e8c57ad2cfe811f6ede
Yomy Gesese <yomygesese@gmail.com>
2022-09-28 02:38:41 -0700

    Python Input/Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,37 +1,30 @@
-!/usr/bin/python3
-""" Module that defines the class Student
+#!/usr/bin/python3
+"""Student module.
+Contains a Student class and some methods.
 """
 
 
-class Student:
-    """ Class to create student instances """
+class Student():
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Sets the necessary attributes for the Student object.
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        obj = self.__dict__.copy()
-        if type(attrs) is list:
-
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
-
-            d_list = {}
-
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return obj
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """ Replaces all attributes of the Student instance """
-        for atr in json:
-            self.__dict__[atr] = json[atr]
+        """Replaces all attributes of the Student instance."""
+        for k, v in json.items():
+            setattr(self, k, v)
